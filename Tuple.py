cc7c7ac5719c122a25136f2b53c59e6ca267a9d1
SAI ARJUN <112474122+SAI-ARJUN@users.noreply.github.com>
2022-09-04 18:49:13 +0530

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,57 @@
+#Indexed
+#tuple=(1,2,3)
+#print(tuple[0])
+
+
+#Ordered
+#t1=(1,2,3)
+#t2=(2,1,3)
+#print(t1==t2)
+
+#Immutable
+#t1=(1,2,3)
+#t1[0]=120
+#print(t1)
+
+#Duplicate
+#t1=(1,2,3,1,3,3)
+#print(t1)
+
+#Iteratable
+#t1=(1,2,3)
+#for i in t1:
+#    print(t1)
+
+#Concatenation of tuple
+#t1=(1,2,3)
+#t2=(2,1,3)
+#print(t1+t2)
+#print(t2*2)
+#print(2 in t1)
+#print(5 in t1)
+#print(len(t1))
+#print(max(t1))
+#print(min(t1))
+
+#Nested tuple
+#t1=(0,1,2,3)
+#t2=('python','java')
+#t3=(t1,t2)
+#print(t3)
+
+#Deleting tuple
+#t3=(0,1)
+#print(t3)
+#del t3
+#print(t3)
+
+
+
+
+
+
+
+
+
+
+
